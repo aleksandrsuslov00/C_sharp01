b0cf859827e0aab111ecafc42d9ebc8d7da7567e
Aleksandr <a.suslov631@gmail.com>
2023-07-03 03:45:41 +0200

    task 10 complete

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberTwo = number  % 100 / 10;   //   Или "NUMBER / 10 % 10"
+if ((number < 100) || (number >= 1000))
+{
+    Console.WriteLine("Число не трёхзначное. Введите трёхзначное число");
+    return;
+}
+Console.WriteLine($"Введено число: {number}");
+Console.WriteLine($"Второе число: {numberTwo}");
